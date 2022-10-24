68b7b68deb83f0b4f14818fde181564bc0eaf213
Nazar <nazar_pilipenko@inbox.ru>
2022-10-24 19:12:18 +0300

    modal

---
+++ script/modal.js
@@ -0,0 +1,51 @@
+
+const modals = () => {
+    function bindModal(triggerSelector, modalSelector, closeSelector) {
+
+        const trigger = document.querySelectorAll(triggerSelector);
+        const modal = document.querySelector(modalSelector);
+        const close = document.querySelector(closeSelector);
+
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (event) => {
+                if(event.target) {
+                    event.preventDefault();
+                }
+                modal.classList.add('show');
+                modal.classList.remove('hide');
+
+                document.body.style.overflow = 'hidden';
+            });
+        });
+        
+        
+        close.addEventListener('click', () => {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+        });
+        
+        modal.addEventListener('click', (event) => {
+            if (event.target === modal) {
+                modal.classList.add('hide');
+                document.body.style.overflow = '';
+
+            }
+        }); 
+    }
+
+    function timeModal(selector, time) {
+        setTimeout(function() {
+            document.querySelector(selector).classList.add('show');
+            document.body.style.overflow = 'hidden';
+
+        }, time);
+    }
+
+
+    bindModal('.header__payment', '.modal', '.modal .close__modal');
+    bindModal('.header__delivery', '.modalDelivery', '.modalDelivery .close__modal');
+    timeModal('.modal', 3000);
+}; 
+
+modals();
